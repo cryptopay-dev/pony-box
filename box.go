66fa7322b88a2478089d4012fa110a0995eb0d7f
Pavel Makarenko <cryfall@gmail.com>
2018-12-10 15:22:56 +0300

    added vendoring added documentation

--- box.go
+++ box.go
@@ -7,10 +7,12 @@ import (
 )
 
 type (
+	// Box is handling dig container
 	Box struct {
 		dig *dig.Container
 	}
 
+	// Provider handles constructor fn and options provided
 	Provider struct {
 		fn   interface{}
 		opts []dig.ProvideOption
@@ -18,9 +20,11 @@ type (
 )
 
 var (
+	// ErrEmptyProviders thrown when empty providers provided
 	ErrEmptyProviders = errors.New("empty providers")
 )
 
+// New creates new Box
 func New() Box {
 	c := dig.New()
 
@@ -29,6 +33,7 @@ func New() Box {
 	}
 }
 
+// NewProvider creates new provider that can be added to box
 func NewProvider(fn interface{}, opts ...dig.ProvideOption) Provider {
 	return Provider{
 		fn:   fn,
@@ -36,6 +41,7 @@ func NewProvider(fn interface{}, opts ...dig.ProvideOption) Provider {
 	}
 }
 
+// Provide setting up a list of providers inside a box, can throw ErrEmptyProviders when providers list is empty.
 func (b Box) Provide(providers ...Provider) error {
 	if len(providers) == 0 {
 		return ErrEmptyProviders
@@ -50,6 +56,7 @@ func (b Box) Provide(providers ...Provider) error {
 	return nil
 }
 
+// Invoke run fn function with all it's dependencies trying to resolve them through box
 func (b Box) Invoke(fn interface{}, opts ...dig.InvokeOption) error {
 	return b.dig.Invoke(fn, opts...)
 }
